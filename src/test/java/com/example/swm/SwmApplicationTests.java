6eac5059e5ef7cb0c7d085ea6ea7eacb8e69e9ea
Emanuele <egalli64@gmail.com>
2025-08-25 12:30:17 +0200

    naming conventions

--- src/test/java/com/example/swm/SwmApplicationTests.java
+++ src/test/java/com/example/swm/SwmApplicationTests.java
@@ -1,4 +1,9 @@
-package com.example.sw;
+/*
+ * Spring Boot Web MVC tutorial 
+ * 
+ * https://github.com/egalli64/spring-mvc
+ */
+package com.example.swm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * Default test - check if the Spring application context loads successfully
  */
 @SpringBootTest
-class SwApplicationTests {
+class SwmApplicationTests {
     @Test
     void contextLoads() {
     }
