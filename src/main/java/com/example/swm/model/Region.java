d0db9db9fe7f1745becefd86c858a11914aeabbb
Emanuele <egalli64@gmail.com>
2025-08-25 11:21:22 +0200

    review

---
+++ src/main/java/com/example/swm/model/Region.java
@@ -0,0 +1,12 @@
+/*
+ * Spring Boot Web MVC tutorial 
+ * 
+ * https://github.com/egalli64/spring-mvc
+ */
+package com.example.swm.model;
+
+/**
+ * A simple DTO
+ */
+public record Region(int id, String name) {
+}
