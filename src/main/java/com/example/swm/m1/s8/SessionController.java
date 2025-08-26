684c724b09b3793708f60f26343dd09a72864474
Emanuele <egalli64@gmail.com>
2025-08-26 19:28:25 +0200

    Refactored from JSP to Thymeleaf interaction

--- src/main/java/com/example/swm/m1/s8/SessionController.java
+++ src/main/java/com/example/swm/m1/s8/SessionController.java
@@ -1,9 +1,14 @@
-package com.example.sw.s8;
+/*
+ * Spring Boot Web MVC tutorial 
+ * 
+ * https://github.com/egalli64/spring-mvc
+ */
+package com.example.swm.m1.s8;
 
 import jakarta.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,26 +16,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/s8")
-public class MockLogger {
-    private static Logger log = LoggerFactory.getLogger(MockLogger.class);
+@RequestMapping("/m1/s8")
+public class SessionController {
+    private static Logger log = LogManager.getLogger(SessionController.class);
 
     @PostMapping("/login")
     public String login(@RequestParam String user, HttpSession session) {
-        log.trace("enter login({})", user);
+        log.traceEntry("login({})", user);
         if (user.isBlank()) {
             log.warn("default user used");
             session.setAttribute("user", "Guest");
         } else {
             session.setAttribute("user", user);
         }
-        return "/s8/home";
+        return "m1/s8";
+    }
+
+    @GetMapping("/guest")
+    public String guest(HttpSession session) {
+        log.traceEntry("guest()");
+
+        session.setAttribute("user", "Guest");
+
+        return "m1/s8";
     }
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
-        log.trace("enter logout()");
+        log.traceEntry("logout()");
         session.invalidate();
-        return "/s8/home";
+        return "m1/s8";
     }
 }
