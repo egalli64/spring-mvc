c9bea1b3207e1590dd352b7757dc8716c3fa1853
Emanuele <egalli64@gmail.com>
2025-08-31 11:39:23 +0200

    A simple DTO

---
+++ src/main/java/com/example/swm/m3/s3/Product.java
@@ -0,0 +1,15 @@
+/*
+ * Spring Boot Web MVC tutorial 
+ * 
+ * https://github.com/egalli64/spring-mvc
+ */
+package com.example.swm.m3.s3;
+
+/**
+ * A simple DTO to be used in a Thymeleaf Variable Expression
+ */
+public record Product(String name, int price) {
+    public String info() {
+        return "Some interesting information about the product " + name;
+    }
+}
