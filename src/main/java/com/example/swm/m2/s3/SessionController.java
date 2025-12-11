9e2ffbd928bd4edd925a2d7efd34db03655e7611
Emanuele <egalli64@gmail.com>
2025-12-11 10:16:04 +0100

    switch to SLF4J

--- src/main/java/com/example/swm/m2/s3/SessionController.java
+++ src/main/java/com/example/swm/m2/s3/SessionController.java
@@ -7,8 +7,8 @@ package com.example.swm.m2.s3;
 
 import jakarta.servlet.http.HttpSession;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/m2/s3")
 public class SessionController {
-    private static Logger log = LogManager.getLogger(SessionController.class);
+    private static final Logger log = LoggerFactory.getLogger(SessionController.class);
 
     @PostMapping("/login")
     public String login(@RequestParam String user, HttpSession session) {
-        log.traceEntry("login({})", user);
+        log.trace("Enter login({})", user);
         if (user.isBlank()) {
             log.warn("default user used");
             session.setAttribute("user", "Guest");
@@ -34,7 +34,7 @@ public class SessionController {
 
     @GetMapping("/guest")
     public String guest(HttpSession session) {
-        log.traceEntry("guest()");
+        log.trace("Enter guest()");
 
         session.setAttribute("user", "Guest");
 
@@ -43,7 +43,7 @@ public class SessionController {
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
-        log.traceEntry("logout()");
+        log.trace("Enter logout()");
         session.invalidate();
         return "m2/s3";
     }
