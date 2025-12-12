02680c8715c4ccb4f52be74c7015d827f260f8bd
Emanuele <egalli64@gmail.com>
2025-12-12 15:48:49 +0100

    Pass through service for HS

---
+++ src/main/java/com/example/swm/hs/ContactService.java
@@ -0,0 +1,39 @@
+/*
+ * Spring Boot Web MVC tutorial - Hypermedia System
+ *
+ * https://github.com/egalli64/spring-mvc
+ */
+package com.example.swm.hs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * A very simple Spring Service
+ */
+@Service
+public class ContactService {
+    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
+
+    private final ContactRepository repo;
+
+    public ContactService(ContactRepository repo) {
+        this.repo = repo;
+    }
+
+    public Optional<Contact> getContact(Long id) {
+        log.trace("Enter getContact({})", id);
+
+        return repo.findById(id);
+    }
+
+    public List<Contact> getContacts() {
+        log.trace("Enter getContacts()");
+
+        return repo.findAll();
+    }
+}
