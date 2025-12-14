083db7d62237fa879eea053e7d912a12087b6279
Emanuele <egalli64@gmail.com>
2025-12-14 21:38:00 +0100

    default ctor

--- src/main/java/com/example/swm/hs/Contact.java
+++ src/main/java/com/example/swm/hs/Contact.java
@@ -13,6 +13,9 @@ public class Contact {
     private String name;
     private String phone;
 
+    public Contact() {
+    }
+
     public Contact(String name, String phone) {
         this.name = name;
         this.phone = phone;
