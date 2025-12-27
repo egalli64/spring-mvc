0448ffbc233a3b5004b8b814b4e264c45c45e7a1
Emanuele <egalli64@gmail.com>
2025-12-27 14:25:34 +0100

    htmx home

--- src/main/java/com/example/swm/hx/HxController.java
+++ src/main/java/com/example/swm/hx/HxController.java
@@ -8,6 +8,7 @@ package com.example.swm.hx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +22,12 @@ import java.time.LocalDateTime;
 @RequestMapping("/hx")
 public class HxController {
     private static final Logger log = LoggerFactory.getLogger(HxController.class);
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("content", "hx/home :: content");
+        return "hx/layout";
+    }
 
     @GetMapping("/get-text")
     @ResponseBody
