a6df55fb533cb7504af10c6e08c4f7021c33a968
Emanuele <egalli64@gmail.com>
2025-12-31 16:06:50 +0100

    adhere to TH fragment common style

--- src/main/java/com/example/swm/hx/HxController.java
+++ src/main/java/com/example/swm/hx/HxController.java
@@ -8,7 +8,6 @@ package com.example.swm.hx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,9 +23,8 @@ public class HxController {
     private static final Logger log = LoggerFactory.getLogger(HxController.class);
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("content", "hx/home :: content");
-        return "hx/layout";
+    public String home() {
+        return "hx/home";
     }
 
     @GetMapping("/get-text")
